d10d2f1f146a4400cf2a7df4e371ff19104e89cc
Aysan Isayo <aisayo@icloud.com>
2018-03-15 17:48:19 -0700

    imported firebase

--- App.js
+++ App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { StyleSheet, Text, View } from 'react-native';
+import reducers from './src/reducers'
+import firebase from 'firebase';
 
 class App extends Component {
+
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyB2WZhmM87JTTHVGpBwow-f9bXgtgUYpjg",
+      authDomain: "manager-36b70.firebaseapp.com",
+      databaseURL: "https://manager-36b70.firebaseio.com",
+      projectId: "manager-36b70",
+      storageBucket: "manager-36b70.appspot.com",
+      messagingSenderId: "354757030018"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello!
@@ -16,4 +31,6 @@ class App extends Component {
     )
   }
 }
+
+export default App;
 
