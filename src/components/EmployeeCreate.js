31e7bcfb6b4ac30538e915b2adc3902c5707af34
Aysan Isayo <aisayo@icloud.com>
2018-03-19 16:01:01 -0700

    added helper method for create employee form submission

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardItem, Input, Button } from './common'
 
 class EmployeeCreate extends Component {
+
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+
+        this.props.employeeCreate({ name, phone, shift });
+    }
+
     render() {
         return (
             <Card>
@@ -45,7 +52,7 @@ class EmployeeCreate extends Component {
                     </CardItem>
 
                 <CardItem>
-                    <Button>
+                    <Button  onPress={this.onButtonPress.bind(this)}>
                         Create
                     </Button>
                 </CardItem>
@@ -67,4 +74,4 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 }
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(EmployeeCreate);
