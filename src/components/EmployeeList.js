67eeddd1a0c34433a56d7fc132bbc6272f5cbbe1
Aysan Isayo <aisayo@icloud.com>
2018-03-21 16:49:27 -0700

    added lifecycles to employee list

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeesFetch();
+
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        //nextprops are the next set of props that this component will
+        //be rendered with
+        this.createDataSource(nextProps)
+    }
+
+    createDataSource({ employees }) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        })
+
+        this.dataSource = ds.cloneWithRows(employees)
+
     }
 
     render() {
