d9e9007a593c320ea132f188263ad9ad80ea4a16
Aysan Isayo <aisayo@icloud.com>
2018-03-21 17:45:40 -0700

    connected employee list to db

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, ListView } from 'react-native';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
+import _ from 'lodash';
+import ListItem from './ListItem'
 
 class EmployeeList extends Component {
     componentWillMount() {
@@ -21,20 +23,31 @@ class EmployeeList extends Component {
             rowHasChanged: (r1, r2) => r1 !== r2
         })
 
-        this.dataSource = ds.cloneWithRows(employees)
+        this.dataSource = ds.cloneWithRows(employees);
 
     }
 
+    renderRow(employee) {
+        return <ListItem employee={employee} />
+    }
+
     render() {
+        console.log("employees list", this.props)
         return (
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
+           <ListView 
+                enableEmptySections
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
         );
     }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return { ...val, uid };
+    })
+        return { employees };
+}
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
